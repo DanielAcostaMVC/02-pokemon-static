5a36f390c4d958a4ee2a0fc0c37e71bd5020b721
Daniel Acosta <daniel.acosta@sinco.com.co>
2023-05-18 18:57:16 -0500

    pokeDras

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { NextUIProvider } from '@nextui-org/react';
+import type { AppProps } from 'next/app'
+import { darktheme } from '../themes'
+import '@/styles/globals.css'
+
+
+function App({ Component, pageProps }: AppProps) {
+  return  (
+    <NextUIProvider theme={darktheme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  )
+  
 }
+
+export default App;
